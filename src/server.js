728583141d3a0405cc27d648efd44519b9018e98
SENIOR\joao.souza <joao.souza@senior.com.br>
2024-12-18 17:32:05 -0300

    Fundamentos de Streams

--- src/server.js
+++ src/server.js
@@ -1,23 +1,27 @@
 import http from 'node:http';
+import { json } from './middlewares/json.js';
 
 const users = [];
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { url, method } = req;
+
+  await json(req, res);
 
   if(method === 'GET' && url === '/users') {
     return res
-    .setHeader('Content-Type', 'application/json')
     .end(JSON.stringify(users));
   }
 
   if(method === 'POST' && url === '/users') {
+    const { name, email } = req.body;
+
     users.push({
       id: 1,
-      name: 'João',
-      email: 'joao.souza@hotmail.com'
+      name,
+      email
     });
-    
+
     return res.writeHead(201).end();
   }
 
